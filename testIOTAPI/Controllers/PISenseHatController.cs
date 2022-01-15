39eea57deb2f2ab77cc3375806ff5bcc9f1e8c5f
Amir Ashraf Ahmad Izham <amirashrafizham@gmail.com>
2022-01-15 15:25:27 +0800

    Added Acceleration and AngularRate

--- testIOTAPI/Controllers/PISenseHatController.cs
+++ testIOTAPI/Controllers/PISenseHatController.cs
@@ -1,11 +1,9 @@
+using Iot.Device.Common;
 using Iot.Device.SenseHat;
 using Microsoft.AspNetCore.Mvc;
 using testIOTAPI.Models;
 
 namespace testIOTAPI.Controllers
-
-
-
 {
     [ApiController]
     [Route("[controller]")]
@@ -18,13 +16,94 @@ namespace testIOTAPI.Controllers
             Weather weather = new Weather();
             using (sh)
             {
-                weather.Temperature1 = sh.Temperature.DegreesCelsius;
-                weather.Temperature2 = sh.Temperature2.DegreesCelsius;
-                weather.Humidity = sh.Humidity.Percent;
-                weather.Pressure = sh.Pressure.Pascals;
+                try
+                {
+                    weather.Temperature1 = Math.Round(sh.Temperature.DegreesCelsius, 2);
+                    weather.Temperature2 = Math.Round(sh.Temperature2.DegreesCelsius, 2);
+                    weather.Humidity = Math.Round(sh.Humidity.Percent, 2);
+                    weather.Pressure = Math.Round(sh.Pressure.Bars, 2);
+                    return Ok(weather);
+                }
+                catch (System.Exception)
+                {
+
+                    return NotFound("try again");
+                }
             }
-            return Ok(weather);
         }
+
+        [HttpGet]
+        public ActionResult<Acceleration> GetAcceleration()
+        {
+            var sh = new SenseHat();
+            Acceleration acceleration = new Acceleration();
+            using (sh)
+            {
+                try
+                {
+                    acceleration.X = sh.Acceleration.X;
+                    acceleration.Y = sh.Acceleration.Y;
+                    acceleration.Z = sh.Acceleration.Z;
+
+                    return Ok(acceleration);
+                }
+                catch (System.Exception)
+                {
+
+                    return NotFound("try again");
+                }
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<AngularRate> GetAngularRate()
+        {
+            var sh = new SenseHat();
+            AngularRate angularRate = new AngularRate();
+            using (sh)
+            {
+                try
+                {
+                    angularRate.X = sh.AngularRate.X;
+                    angularRate.Y = sh.AngularRate.Y;
+                    angularRate.Z = sh.AngularRate.Z;
+
+                    return Ok(angularRate);
+                }
+                catch (System.Exception)
+                {
+
+                    return NotFound("try again");
+                }
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<MagneticInduction> GetMagneticInduction()
+        {
+            var sh = new SenseHat();
+            MagneticInduction magneticInduction = new MagneticInduction();
+            using (sh)
+            {
+                try
+                {
+                    magneticInduction.X = sh.AngularRate.X;
+                    magneticInduction.Y = sh.AngularRate.Y;
+                    magneticInduction.Z = sh.AngularRate.Z;
+
+                    return Ok(magneticInduction);
+                }
+                catch (System.Exception)
+                {
+
+                    return NotFound("try again");
+                }
+            }
+        }
+
+
+
+
 
     }
 }
